c6f151c0b0ce8d6e8a2de07c1d8ae6ded8ff9773
DEVELoper <syabblogger@gmail.com>
2022-11-22 22:05:25 +0500

    initial commit

---
+++ list.py
@@ -0,0 +1,8 @@
+print("Lists in pytghon By DEveloper")
+mylist = ["Syed", "SYab", "Ahmad", "Shah"]
+print(mylist[1:])
+print(mylist[:2])
+print(mylist[3:])
+print(mylist[1:3])
+print(mylist[-1:])
+print(mylist[:-2])
--- string.py
+++ string.py
@@ -1,5 +1,8 @@
 print("String by DEveloper")
-data = "Developer"
+data = "Developer number {}"
+info = "Syab Ahmad de "
+number = 1
+infoData = info + data
 print(len(data))
 print(type(data))
 print(data[1])
@@ -9,4 +12,7 @@ print(data[:2])
 print(data.upper())
 print(data.lower())
 print(data.replace("developer", "Syab Ahmad de Developer"))
-print(data)
+print(data.strip())
+print(info, data)
+print(infoData)
+print(infoData.format(number))
