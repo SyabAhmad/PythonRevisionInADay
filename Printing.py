070731692c72ccdcb718f474d519694c8ae7a308
DEVELoper <syabblogger@gmail.com>
2022-11-22 21:30:15 +0500

    initial commit

---
+++ Printing.py
@@ -0,0 +1,7 @@
+
+print("Hello From Developer!!")
+print("""
+    in this Printing Script of Python, we will gonna see through
+    the printing code it means how can we print something
+     or display something in the Console
+    """)
