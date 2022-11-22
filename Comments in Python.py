94006beeb83c58e9683bc7ec72d53d80914c970f
DEVELoper <syabblogger@gmail.com>
2022-11-22 21:37:14 +0500

    initial commit

---
+++ Comments in Python.py
@@ -0,0 +1,16 @@
+print("""
+    in this Script we will gonna talk about Comments in Python
+    there are TWO/2 Types of Comments
+    ✔Single type Comments
+    ✔ Multiline Comments
+    Single Line Comments are represented or followed by #
+    Multiline Comments are represented or followed between 
+    the """""" like Given Below
+    """)
+
+# this is Single Line Comment
+#this onother Single Line Comment
+# and This the #rd Single Line Comments
+"""and these are the 
+Multiline Comments in 
+Python"""
