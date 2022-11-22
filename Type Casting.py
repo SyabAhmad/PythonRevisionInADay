9bc53e3dce4b645748c88f31d2d7782ba297dcc6
DEVELoper <syabblogger@gmail.com>
2022-11-22 21:47:32 +0500

    initial commit

---
+++ Type Casting.py
@@ -0,0 +1,5 @@
+print("Type Casting By DEveloper")
+x = int("12")
+y = 21
+result = x + y
+print("Result" , result)
---
+++ string.py
@@ -0,0 +1,8 @@
+print("String by DEveloper")
+data = "Developer"
+print(len(data))
+print(type(data))
+print(data[1])
+print(data[2:])
+print(data[2:4])
+print(data[:2])
