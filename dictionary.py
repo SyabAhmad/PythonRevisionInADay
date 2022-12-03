f17a2e92aa30a6ae929b8d4db9a7a9228cd6cf9a
DEVELoper <syabblogger@gmail.com>
2022-12-03 21:16:23 +0500

    initial commit

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+print("Dictionary in Python")
+myDictionary = {"apple":"2 Dozen", "banana": "3 Dozen", "Orange": "7 Dozen"}
+print(myDictionary)
+
+# in this case Apple is a Key and 2 Dozen is the value.
+
+print(myDictionary["apple"])
+print(myDictionary["banana"])
+print(myDictionary["Orange"])
+
+print(type(myDictionary))
+print(len(myDictionary))
+
+#dictionary using dict method
+
+myDictionary1 = dict(name = "Syab Ahmad", age = "23", semester = "5TH")
+print(myDictionary1)
+print(type(myDictionary))
+print(len(myDictionary))
