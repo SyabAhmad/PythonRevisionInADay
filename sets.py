df3f3641cb787c090b40e20d8c06278df436f160
DEVELoper <syabblogger@gmail.com>
2022-12-03 21:09:14 +0500

    initial commit

--- sets.py
+++ sets.py
@@ -1,14 +1,11 @@
 print("Sets in python")
 
-numbers = [1,2,3,4,5,6,7]
+numbers = {1,2,3,4,5,6,7}
 
+print(type(numbers))
 
-numbers.append(1)
-numbers.append(3)
-numbers.append(9)
 print(numbers)
-numbers.sort()
+
 print(numbers)
 print(len(numbers))
-print(numbers[3:])
-print(numbers[:-4])
+
