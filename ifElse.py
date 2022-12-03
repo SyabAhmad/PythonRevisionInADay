e2261af2be2b44ed7f60e0090f46838524e8acdf
DEVELoper <syabblogger@gmail.com>
2022-12-03 21:21:19 +0500

    initial commit

---
+++ ifElse.py
@@ -0,0 +1,8 @@
+print("if and Else in Python")
+age = 31
+if age >= 18 and age <= 30:
+    print("you are now adult")
+elif age > 30 and age < 50:
+    print("you are Old")
+else:
+    print("you are kid")
