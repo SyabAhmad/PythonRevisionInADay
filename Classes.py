94b8f52d57c2be4cecc34b63b5d9150757a703af
DEVELoper <syabblogger@gmail.com>
2022-12-05 20:55:25 +0500

    initial commit

---
+++ Classes.py
@@ -0,0 +1,13 @@
+import this
+
+
+class student:
+    def __int__(self, name, age, rollNumber):
+        this.name = name
+        this.age = age
+        this.rollNumber = rollNumber
+
+std = student("developer", 22, 206174)
+
+print(std)
+
---
+++ input.py
@@ -0,0 +1,7 @@
+print("input data in python")
+username = input("Enter your Username: ")
+pinCode = input("Enter Your Pin Code: ")
+if int(pinCode) == 1234:
+    print("Welcome Bro")
+else:
+    print("Incorrect Pin COde or Username")
