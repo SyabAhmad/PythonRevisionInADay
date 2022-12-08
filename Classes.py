796569598a1f1061fb4f552cd15e8b5b16df2db8
DEVELoper <syabblogger@gmail.com>
2022-12-08 20:11:11 +0500

    Classes or added

--- Classes.py
+++ Classes.py
@@ -7,6 +7,14 @@ class student:
 
  # self  keyword is used for accessing variables of the same class
 std = student("de Developer")
-
 std.displayUsername()
-
+#to modify class properties we can do that like this ...
+std.name = "DE Developer"
+std.displayUsername()
+# to delete class properties we can do that like this ...
+del std.name
+std.displayUsername()
+# we can also delete the object of the class
+del std
+std.displayUsername()
+#now it will show error
